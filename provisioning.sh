2431ed72fa484724b02e165737c53411358bce0c
hotaru <36354847+hotaru51@users.noreply.github.com>
2019-01-14 16:35:24 +0900

    provisioning.sh修正

--- provisioning.sh
+++ provisioning.sh
@@ -9,10 +9,8 @@ apt install -y \
   curl \
   software-properties-common
 
-# GPGkキー追加
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
 # Dockerリポジトリ追加
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository -y \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
